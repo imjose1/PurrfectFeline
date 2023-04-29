4c05ecc0bccf967335aaeb5e6b9c2d2db5cf50b5
Jose g hernandez <jose_ghj@hotmail.com>
2023-04-28 22:01:04 -0500

    added controller

---
+++ Controllers/BreedsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PurrfectFeline.Controllers
+{
+    public class BreedsController:Controller
+    {
+        public IActionResult Index()
+        {
+            return View("Breeds");
+        }
+    }
+}
---
+++ Controllers/CatsListController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PurrfectFeline.Controllers
+{
+    public class CatsListController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View("CatsList");
+        }
+    }
+}
